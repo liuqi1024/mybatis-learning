e8228b31d06157d367b33aaa4b03280f93bb4170
liuqi1024 <8655813@qq.com>
2021-01-18 10:01:18 +0800

    add:user sql

---
+++ myb-learning/src/main/java/com/gw/ken/myb/MybLearningApplication.java
@@ -0,0 +1,13 @@
+package com.gw.ken.myb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybLearningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MybLearningApplication.class, args);
+  }
+
+}
---
+++ myb-learning/src/test/java/com/gw/ken/myb/MybLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gw.ken.myb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybLearningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
